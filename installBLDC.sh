4c114f7efd9d6b1b919611f912121a826802c9e2
YundoChoi <48561168+zygn@users.noreply.github.com>
2020-08-19 13:48:15 +0900

    Add files via upload

---
+++ installBLDC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "install BLDC-Tools"
+echo "[1/4] Install Libraries..."
+sudo apt-get install qtcreator libqt4-dev libudev-dev libqt5serialport5-dev -y
+echo "[2/4] Download BLDC-Tools..."
+git clone https://github.com/zygn/bldc-tool
+cd bldc-tool
+echo "[3/4] Compiling BLDC-Tools..."
+qmake -qt=qt5
+make clean && make
+echo "[4/4] System Setup..."
+sudo adduser $USER dialout
+
+echo ""
+echo "Install Complete!!!"
+echo ""
+
